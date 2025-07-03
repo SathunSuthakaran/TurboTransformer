9065bf51efd07907c9c6d7a294bff8994deda9c0
sschemist <Sathun@gmail.com>
2025-07-03 11:34:11 -0400

    implemented benchmarking (after a few mins of runtime  + hot computer)

--- model_loader.py
+++ model_loader.py
@@ -1,24 +1,20 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
 
-def run_model():
-    quant_path = "quant-path"
-    id = "Qwen/Qwen1.5-1.8B" #relatively small in terms of paramters
-    tokenizer = AutoTokenizer.from_pretrained(id, trust_remote_code=True)
-    model = AutoModelForCausalLM.from_pretrained(id, trust_remote_code=True).to('cpu')
-    prompt = 'who is the best poker player in the world'
+id = "Qwen/Qwen1.5-1.8B" #relatively small in terms of paramters
+
+def load_tokenizer():
+    return AutoTokenizer.from_pretrained(id, trust_remote_code=True)
+
+def load_model():
+    return AutoModelForCausalLM.from_pretrained(id, trust_remote_code=True).to('cpu')
+
+def load_quantized(model=None):
+    if not model:
+        model = load_model()
     quantized_model = torch.quantization.quantize_dynamic(
         model,
         {torch.nn.Linear},
         dtype=torch.qint8
         )
-    inputs = tokenizer(prompt, return_tensors="pt").to('cpu')
-    
-    with torch.no_grad():
-        output = quantized_model.generate(**inputs, max_new_tokens = 150)
-        print(tokenizer.decode(output[0], skip_special_tokens=True))
-    
-if __name__ == '__main__':
-    run_model()
-    
-    
+    return quantized_model    
